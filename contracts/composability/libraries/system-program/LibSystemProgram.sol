f2664422b77f986c061a425c23ab9dc1697a77b1
Maxime Suard <maxpolizzo@gmail.com>
2025-03-13 18:05:06 +0100

    Added: System program's transfer, assignWithSeed and allocateWithSeed instructions formatting, execution logic and test cases

--- contracts/composability/libraries/system-program/LibSystemProgram.sol
+++ contracts/composability/libraries/system-program/LibSystemProgram.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.28;
 
 import { SolanaDataConverterLib } from "../../../utils/SolanaDataConverterLib.sol";
+import { LibSystemData } from "./LibSystemData.sol";
 
 /// @title LibSystemProgram
 /// @notice Helper library for interactions with Solana's System program
@@ -30,7 +31,7 @@ library LibSystemProgram {
     ) {
         accounts = new bytes32[](3);
         accounts[0] = payer;
-        accounts[1] = getCreateWithSeedAccount(basePubKey, programId, seed); // Account to be created
+        accounts[1] = LibSystemData.getCreateWithSeedAccount(basePubKey, programId, seed); // Account to be created
         accounts[2] = basePubKey;
 
         isSigner = new bool[](3);
@@ -48,7 +49,7 @@ library LibSystemProgram {
         bytes32 rentExemptBalanceLE = bytes32(SolanaDataConverterLib.readLittleEndianUnsigned256(uint256(rentExemptBalance)));
         bytes32 accountSizeLE = bytes32(SolanaDataConverterLib.readLittleEndianUnsigned256(uint256(accountSize)));
         data = abi.encodePacked(
-            bytes4(0x03000000), // Instruction variant
+            bytes4(0x03000000), // Instruction variant (see: https://github.com/solana-program/system/blob/17d70bc0e56354cc7811e22a28776e7f379bcd04/interface/src/instruction.rs#L121)
             basePubKey, // Base public key used for account  creation
             bytes8(seedLenLE), // Seed bytes length (right-padded little-endian)
             seed, // Seed bytes
@@ -58,17 +59,117 @@ library LibSystemProgram {
         );
     }
 
-    /// @notice Helper function to derive the address of the Solana account which would be created by executing a
-    /// `createAccountWithSeed` instruction formatted with the same parameters
-    /// @param basePubKey The base public key used to derive the newly created account
-    /// @param programId The id of the Solana program which would be granted permission to write data to the newly
-    /// created account
-    /// @param seed The bytes seed used to derive the newly created account
-    function getCreateWithSeedAccount(
+    /// @notice Helper function to format a `transfer` instruction to transfer SOL
+    /// @param sender The account which will send SOL
+    /// @param recipient The account which will receive SOL
+    /// @param amount The amount to transfer
+    function formatTransferInstruction(
+        bytes32 sender,
+        bytes32 recipient,
+        uint64 amount
+    ) internal pure returns (
+        bytes32[] memory accounts,
+        bool[] memory isSigner,
+        bool[] memory isWritable,
+        bytes memory data
+    ) {
+        accounts = new bytes32[](2);
+        accounts[0] = sender;
+        accounts[1] = recipient;
+
+        isSigner = new bool[](2);
+        isSigner[0] = true;
+        isSigner[1] = false;
+
+        isWritable = new bool[](2);
+        isWritable[0] = true;
+        isWritable[1] = true;
+
+        // Get amount in right-padded little-endian bytes format
+        bytes32 amountLE = bytes32(SolanaDataConverterLib.readLittleEndianUnsigned256(uint256(amount)));
+        data = abi.encodePacked(
+            bytes4(0x02000000), // Instruction variant (see: https://github.com/solana-program/system/blob/17d70bc0e56354cc7811e22a28776e7f379bcd04/interface/src/instruction.rs#L111)
+            bytes8(amountLE) // Amount (right-padded little-endian)
+        );
+    }
+
+    /// @notice Helper function to format a `assignWithSeed` instruction to assign a Solana account to a Solana program
+    /// @param basePubKey The base public key used to derive the account that we want to assign
+    /// @param programId The public key of the program that was used to derive the account and to which we want to
+    /// assign the account
+    /// @param seed The bytes seed that was used to generate the account that we want to assign
+    function formatAssignWithSeedInstruction(
         bytes32 basePubKey,
         bytes32 programId,
         bytes memory seed
-    ) internal pure returns(bytes32) {
-        return sha256(abi.encodePacked(basePubKey, seed, programId));
+    ) internal pure returns (
+        bytes32[] memory accounts,
+        bool[] memory isSigner,
+        bool[] memory isWritable,
+        bytes memory data
+    ) {
+        accounts = new bytes32[](2);
+        accounts[0] = LibSystemData.getCreateWithSeedAccount(basePubKey, programId, seed); // Account to be assigned
+        accounts[1] = basePubKey;
+
+        isSigner = new bool[](2);
+        isSigner[0] = false;
+        isSigner[1] = true;
+
+        isWritable = new bool[](2);
+        isWritable[0] = true;
+        isWritable[1] = false;
+
+        // Get seed length value in right-padded little-endian bytes format
+        bytes32 seedLenLE = bytes32(SolanaDataConverterLib.readLittleEndianUnsigned256(seed.length));
+        data = abi.encodePacked(
+            bytes4(0x0a000000), // Instruction variant (see: https://github.com/solana-program/system/blob/17d70bc0e56354cc7811e22a28776e7f379bcd04/interface/src/instruction.rs#L216)
+            basePubKey,
+            bytes8(seedLenLE), // Seed bytes length (right-padded little-endian)
+            seed, // Seed bytes
+            programId // program id
+        );
+    }
+
+    /// @notice Helper function to format a `allocateWithSeed` instruction to allocate storage space to a Solana account
+    /// @param basePubKey The base public key used to derive the account that we want to allocate space to
+    /// @param programId The public key of the program that was used to derive the account that we want to allocate
+    /// space to
+    /// @param seed The bytes seed that was used to generate the account that we want to allocate space to
+    /// @param accountSize The on-chain storage space that we ant to allocate to the account
+    function formatAllocateWithSeedInstruction(
+        bytes32 basePubKey,
+        bytes32 programId,
+        bytes memory seed,
+        uint64 accountSize
+    ) internal pure returns (
+        bytes32[] memory accounts,
+        bool[] memory isSigner,
+        bool[] memory isWritable,
+        bytes memory data
+    ) {
+        accounts = new bytes32[](2);
+        accounts[0] = LibSystemData.getCreateWithSeedAccount(basePubKey, programId, seed); // Account to be assigned
+        accounts[1] = basePubKey;
+
+        isSigner = new bool[](2);
+        isSigner[0] = false;
+        isSigner[1] = true;
+
+        isWritable = new bool[](2);
+        isWritable[0] = true;
+        isWritable[1] = false;
+
+        // Get values in right-padded little-endian bytes format
+        bytes32 seedLenLE = bytes32(SolanaDataConverterLib.readLittleEndianUnsigned256(seed.length));
+        bytes32 accountSizeLE = bytes32(SolanaDataConverterLib.readLittleEndianUnsigned256(uint256(accountSize)));
+        data = abi.encodePacked(
+            bytes4(0x09000000), // Instruction variant (see: https://github.com/solana-program/system/blob/17d70bc0e56354cc7811e22a28776e7f379bcd04/interface/src/instruction.rs#L197)
+            basePubKey,
+            bytes8(seedLenLE), // Seed bytes length (right-padded little-endian)
+            seed, // Seed bytes
+            bytes8(accountSizeLE), // Storage space for created account (right-padded little endian)
+            programId // program id
+        );
     }
 }
